93d6da55af58de065c6fffe69ecd38fe2ddae674
keyrun99 <146376060+keyrun99@users.noreply.github.com>
2024-01-22 17:53:30 -0500

    Update clustering.js

--- clustering/clustering.js
+++ clustering/clustering.js
@@ -1,6 +1,6 @@
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 3,
+    zoom: 6,
     center: { lat: 49.758, lng: -92.653 },
   });
   // Create an array of alphabetical characters used to label the markers.
