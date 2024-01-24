1ab3d6231724dcde8264074ab5581a6efbf5d47b
keyrun99 <146376060+keyrun99@users.noreply.github.com>
2024-01-24 17:37:22 -0500

    Update index.js

--- story/index.js
+++ story/index.js
@@ -8,7 +8,7 @@ async function initMap() {
     zoom: 7,
 
     // "Administrative Area Level 1" means the map tragets the states, province or region on feature layer.
-    mapId: "7ba16be0c9375fa7",
+    //mapId: "7ba16be0c9375fa7",
   });
   const featureLayer = map.getFeatureLayer(
     google.maps.FeatureType.ADMINISTRATIVE_AREA_LEVEL_1,
