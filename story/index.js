bb0dc1932705a6f715b0068a4d3ee2e72e621d67
keyrun99 <146376060+keyrun99@users.noreply.github.com>
2024-01-25 23:47:42 -0500

    Update index.js

--- story/index.js
+++ story/index.js
@@ -1,6 +1,7 @@
 // https://developers.google.com/maps/documentation/javascript/dds-boundaries/choropleth-map#try-sample
-// This example displays a marker at the head office of China communication service International Nepal.
-// When the user clicks the marker, an info window opens.
+// https://jsfiddle.net/gh/get/library/pure/googlemaps/js-samples/tree/master/dist/samples/infowindow-simple/jsfiddle
+// This example displays a marker at the head office of China Communication Service International Nepal.
+// The marker is located at the China communication service head office and When the user clicks the marker, an info window opens.
 
 function initMap() {
   // Request needed Libraries
@@ -8,10 +9,11 @@ function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 7,
     center: center,
-
-    // "Administrative Area Level 1" means the map tragets the states, province or region on feature layer.
     mapId: "7ba16be0c9375fa7",
   });
+
+  // "Administrative Area Level 1" means the map tragets the states, province or region on feature layer.
+  
   const featureLayer = map.getFeatureLayer(
     google.maps.FeatureType.ADMINISTRATIVE_AREA_LEVEL_1,
   );
@@ -23,10 +25,8 @@ function initMap() {
     let fillColor;
 
     // Specify colors using any of the following:
-    // * Named ('green')
     // * Hexadecimal ('#FF0000')
-    // * RGB ('rgb(0, 0, 255)')
-    // * HSL ('hsl(60, 100%, 50%)')
+
     if (towercount < 200) {
       fillColor = "#58CCED";
     } else if (towercount < 300) {
@@ -42,7 +42,7 @@ function initMap() {
     };
   };
   
-  // towercount by state of nepal supervised by me
+  // towercount by the state of Nepal supervised by me
 
   const states = {
     "ChIJJTMWst7W6DkRYeNF5qo7HsY": 300, // Koshi Province
@@ -52,9 +52,9 @@ function initMap() {
     "ChIJgQJOSit5ljkR6lK-Oz3cNcg": 200, // Lumbini Province
     "ChIJlSRJE2TGvDkRAFmPDaDubQY": 190, // Karnali Province
     "ChIJm4QKVy1ZoTkR6IGi80pLKog": 400, // Suderpaschim Province
-    
     }
-    
+
+  //marker info
   const contentString =
     '<div id="content">' +
     '<div id="siteNotice">' +
@@ -75,7 +75,6 @@ function initMap() {
   const marker = new google.maps.Marker({
     position: center,
     map,
-    title: "center (Ayers Rock)",
   });
 
   marker.addListener("click", () => {
